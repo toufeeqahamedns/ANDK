ce4b9d9841dee33ecbdefbb89bd5b9119ffa5a5d
toufeeqahamedns <toufeeqahamedns@gmail.com>
2020-11-25 14:29:24 +0530

    Adding Images

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.example.android.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -10,18 +12,34 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.rollButton.text = "Let's Roll"
+        diceImage = binding.diceImage
+        rollButton = binding.rollButton
 
-        binding.rollButton.setOnClickListener {
+        rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        binding.resultText.text = (Random.nextInt(6) + 1).toString()
+
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
